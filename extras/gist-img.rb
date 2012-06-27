d00d249d0105e7d7672d44c75798ee6348a8344b
Chris Mytton <self@hecticjeff.net>
2012-06-27 01:13:41 +0100

    Install tagged version with homebrew

--- extras/gist-img.rb
+++ extras/gist-img.rb
@@ -2,8 +2,10 @@
 require 'formula'
 
 class GistImg < Formula
+  url 'https://github.com/hecticjeff/gist-img/zipball/v0.0.1'
   homepage 'https://github.com/hecticjeff/gist-img'
   head 'https://github.com/hecticjeff/gist-img.git'
+  md5 'd98ee80e3cd3a0c0c42097210810bffc'
 
   depends_on 'gist'
 
