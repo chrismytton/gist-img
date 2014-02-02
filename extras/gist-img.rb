5161aee7376e508f5824f7d3085ef2fece712de9
Chris Mytton <self@hecticjeff.net>
2014-02-02 09:08:45 +0000

    Update homebrew formula
    Closes #2

--- extras/gist-img.rb
+++ extras/gist-img.rb
@@ -2,10 +2,10 @@
 require 'formula'
 
 class GistImg < Formula
-  url 'https://github.com/hecticjeff/gist-img/zipball/v0.0.1'
+  url 'https://github.com/hecticjeff/gist-img/archive/v0.0.1.zip'
   homepage 'https://github.com/hecticjeff/gist-img'
   head 'https://github.com/hecticjeff/gist-img.git'
-  md5 'd98ee80e3cd3a0c0c42097210810bffc'
+  sha256 '168213f91284e740356a0df55ff6f6c203b128f95d5662124e1b53113c1625bf'
 
   depends_on 'gist'
 
