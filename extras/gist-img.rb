9904b9affd017268cfdbfb487810da74195fcb22
Chris Mytton <chrismytton@gmail.com>
2015-09-04 22:51:27 +0100

    Fix sha for homebrew formula
    Fixes #6

--- extras/gist-img.rb
+++ extras/gist-img.rb
@@ -5,7 +5,7 @@ class GistImg < Formula
   url 'https://github.com/hecticjeff/gist-img/archive/v0.0.1.zip'
   homepage 'https://github.com/hecticjeff/gist-img'
   head 'https://github.com/hecticjeff/gist-img.git'
-  sha256 '168213f91284e740356a0df55ff6f6c203b128f95d5662124e1b53113c1625bf'
+  sha256 '6d3b95fe70d5a03b3e3cd52d92fb2f9f1e2ba28cf105d20bbaefc017f43c46f3'
 
   depends_on 'gist'
 
