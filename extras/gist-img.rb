5c54f3f288e771f6f30dba70fd7e51d2e42f58ed
Chris Mytton <chrismytton@gmail.com>
2015-09-04 22:52:53 +0100

    s/hecticjeff/chrismytton

--- extras/gist-img.rb
+++ extras/gist-img.rb
@@ -2,9 +2,9 @@
 require 'formula'
 
 class GistImg < Formula
-  url 'https://github.com/hecticjeff/gist-img/archive/v0.0.1.zip'
-  homepage 'https://github.com/hecticjeff/gist-img'
-  head 'https://github.com/hecticjeff/gist-img.git'
+  url 'https://github.com/chrismytton/gist-img/archive/v0.0.1.zip'
+  homepage 'https://github.com/chrismytton/gist-img'
+  head 'https://github.com/chrismytton/gist-img.git'
   sha256 '6d3b95fe70d5a03b3e3cd52d92fb2f9f1e2ba28cf105d20bbaefc017f43c46f3'
 
   depends_on 'gist'
